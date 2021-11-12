549ed99418898e75da1aece3a6dff1ed6d0f9c68
Jerimiah Willhite <>
2021-11-11 22:35:14 -0800

    add ui files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// create a fileserver which serves files out of the static directory:
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() function to register the file server as the handler for all URL paths that start with "/static/".
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
